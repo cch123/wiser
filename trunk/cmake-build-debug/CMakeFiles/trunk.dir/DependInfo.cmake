237730d7e290d392e17e0ce4ed76e95db21fac2b
Xargin <cao1988228@163.com>
2017-02-08 20:17:17 +0800

    change readme

---
+++ trunk/cmake-build-debug/CMakeFiles/trunk.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Xargin/wiser/trunk/database.c" "/Users/Xargin/wiser/trunk/cmake-build-debug/CMakeFiles/trunk.dir/database.c.o"
+  "/Users/Xargin/wiser/trunk/postings.c" "/Users/Xargin/wiser/trunk/cmake-build-debug/CMakeFiles/trunk.dir/postings.c.o"
+  "/Users/Xargin/wiser/trunk/search.c" "/Users/Xargin/wiser/trunk/cmake-build-debug/CMakeFiles/trunk.dir/search.c.o"
+  "/Users/Xargin/wiser/trunk/token.c" "/Users/Xargin/wiser/trunk/cmake-build-debug/CMakeFiles/trunk.dir/token.c.o"
+  "/Users/Xargin/wiser/trunk/util.c" "/Users/Xargin/wiser/trunk/cmake-build-debug/CMakeFiles/trunk.dir/util.c.o"
+  "/Users/Xargin/wiser/trunk/wikiload.c" "/Users/Xargin/wiser/trunk/cmake-build-debug/CMakeFiles/trunk.dir/wikiload.c.o"
+  "/Users/Xargin/wiser/trunk/wiser.c" "/Users/Xargin/wiser/trunk/cmake-build-debug/CMakeFiles/trunk.dir/wiser.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ trunk/cmake-build-debug/CMakeFiles/trunk.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trunk.dir/database.c.o"
+  "CMakeFiles/trunk.dir/postings.c.o"
+  "CMakeFiles/trunk.dir/search.c.o"
+  "CMakeFiles/trunk.dir/token.c.o"
+  "CMakeFiles/trunk.dir/util.c.o"
+  "CMakeFiles/trunk.dir/wikiload.c.o"
+  "CMakeFiles/trunk.dir/wiser.c.o"
+  "trunk.pdb"
+  "trunk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trunk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
